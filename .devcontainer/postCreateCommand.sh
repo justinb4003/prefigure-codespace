5de4d78d0dd00b34ddd5aad5999b42c76da91a36
David Austin <david.austin.m@gmail.com>
2024-09-16 15:58:46 -0400

    back to ubuntu

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-cat .my_bashrc >> ~/.bashrc
-. ~/.bashrc
-echo '/usr/lib/python3/dist-packages' > /usr/local/lib/python3.10/site-packages/louis.pth
 prefig init
 prefig new
 prefig examples
+cat .my_bashrc >> ~/.bashrc
+. ~/.bashrc
 
